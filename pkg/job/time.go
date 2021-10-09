55573f335625b2826a0bb70e6eb11a07b924463d
John Niang <johnniang@fastmail.com>
2021-10-09 16:04:20 +0800

    Add MarshalJSON method for job.time type

--- pkg/job/time.go
+++ pkg/job/time.go
@@ -27,10 +27,28 @@ func (t *Time) UnmarshalJSON(data []byte) error {
 	if err := json.Unmarshal(data, &timeStr); err != nil {
 		return err
 	}
+	// parse using Jenkins time layout first
 	parsedTime, err := time.Parse(jenkinsTimeLayout, timeStr)
 	if err != nil {
+		// parse using RFC3339 layout corresponding to MarshalJSON the timeStr isn't sufficient Jenkins time layout
+		parsedTime, err = time.Parse(time.RFC3339, timeStr)
+	}
+	if err != nil {
+		// return error if we tried the above methods
 		return err
 	}
 	t.Time = parsedTime.Local()
 	return nil
+}
+
+// MarshalJSON implements the json.Marshaler interface.
+func (t *Time) MarshalJSON() ([]byte, error) {
+	if t.IsZero() {
+		return []byte("null"), nil
+	}
+	buf := make([]byte, 0, len(time.RFC3339)+2)
+	buf = append(buf, '"')
+	buf = t.UTC().AppendFormat(buf, time.RFC3339)
+	buf = append(buf, '"')
+	return buf, nil
 }
